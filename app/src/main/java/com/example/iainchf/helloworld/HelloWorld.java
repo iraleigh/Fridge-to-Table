79f804da36c632ebb2355ab452177fb74da1e8b6
Aura-Davis <auramisc@yahoo.com>
2015-10-06 00:08:30 -0700

    Aura pushed Hello World to the repository

--- app/src/main/java/com/example/iainchf/helloworld/HelloWorld.java
+++ app/src/main/java/com/example/iainchf/helloworld/HelloWorld.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-//Nwilley91 testing commit and puch.
+//Nwilley91 testing commit and push.
 public class HelloWorld extends AppCompatActivity {
 
     @Override
@@ -16,6 +16,7 @@ public class HelloWorld extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
+
         getMenuInflater().inflate(R.menu.menu_hello_world, menu);
         return true;
     }
