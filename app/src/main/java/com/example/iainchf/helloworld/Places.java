9f94666c1871bcd163a781479b4a990fb10d70e6
alvinlu <alvinspeakstoo@gmail.com>
2015-11-10 12:32:15 -0800

    pushed Places & gPlacesAPI

---
+++ app/src/main/java/com/example/iainchf/helloworld/Places.java
@@ -0,0 +1,14 @@
+package com.example.iainchf.helloworld;
+
+import java.util.*;
+
+/**
+ * Created by alvinlu on 11/9/15.
+ */
+public class Places {
+    private String name;
+    private String address;
+    private double lat;
+    private double lng;
+
+}
---
+++ app/src/main/java/com/example/iainchf/helloworld/gPlacesAPI.java
@@ -0,0 +1,21 @@
+package com.example.iainchf.helloworld;
+
+/**
+ * Created by alvinlu on 11/10/15.
+ */
+import android.annotation.TargetApi;
+import android.os.Build;
+import android.util.JsonReader;
+import android.util.JsonToken;
+import android.util.Log;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.StringReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class gPlacesAPI {
+
+}
